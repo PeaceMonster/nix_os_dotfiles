0bbf70baadc13e4d6c61f268443c6b2c0adf6aa9
Mikkel Ugilt <ugilt3@gmail.com>
2024-09-02 10:49:06 +0200

    theming

--- user/theming.nix
+++ user/theming.nix
@@ -1,5 +1,8 @@
-{...}: {
+{pkgs, ...}: {
   stylix.enable = true;
 
   stylix.image = ./images/starStable.png;
+  stylix.polarity = "dark";
+
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-soft.yaml";
 }
