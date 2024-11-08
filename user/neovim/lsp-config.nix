a38f4a032a84ecb738a80165d6099cdf04bac888
Mikkel Ugilt <ugilt3@gmail.com>
2024-11-08 09:29:02 +0100

    Rename

--- user/neovim/lsp-config.nix
+++ user/neovim/lsp-config.nix
@@ -1,20 +1,20 @@
 {...}: {
   programs.nixvim.plugins.lsp.servers = {
     nixd.enable = true;
-    rust-analyzer = {
+    rust_analyzer = {
       enable = true;
       installCargo = false;
       installRustc = false;
     };
     metals.enable = true;
-    typst-lsp.enable = true;
-    jdt-language-server.enable = true;
+    typst_lsp.enable = true;
+    jdt_language-server.enable = true;
     cssls.enable = true;
     zls.enable = true;
     # sourcekit.enable = true;
     # ccls.enable = true;
     clangd.enable = true;
-    ts-ls.enable = true;
+    ts_ls.enable = true;
   };
 
   programs.nixvim.extraConfigLua = ''
