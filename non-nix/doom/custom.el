8b95d634b5f919477050777fefc62512712e4fc2
Mikkel Ugilt <ugilt3@gmail.com>
2025-05-08 08:51:02 +0200

    Added doom directly

---
+++ non-nix/doom/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("691d671429fa6c6d73098fc6ff05d4a14a323ea0a18787daeb93fde0e48ab18b" default))
+ '(org-agenda-files
+   '("~/org/HCI/HCI.org" "/home/ugilt/org/Agenda.org" "/home/ugilt/org/Spoppydataloger.org" "/home/ugilt/org/TestFile.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
