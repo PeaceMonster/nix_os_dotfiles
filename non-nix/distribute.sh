0def04f73ba033c4ecd4d54a6c30470189885fb2
Mikkel Ugilt <ugilt3@gmail.com>
2025-05-14 09:37:20 +0200

    Quick distribute script

---
+++ non-nix/distribute.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+ln -s $NON_NIX/doom $HOME/.config/doom
+ln -s $NON_NIX/qtile $HOME/.config/qtile
+ln -s $NON_NIX/qutebrowser $HOME/.config/qutebrowser
+ln -s $NON_NIX/qute-containers $HOME/.local/share/qute-containers
