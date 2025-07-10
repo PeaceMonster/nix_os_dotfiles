35edbe866e3bbc5d033b8438ed8560d53f251bc4
Mikkel Ugilt <ugilt3@gmail.com>
2025-07-10 17:39:27 +0200

    Update distribute script

--- non-nix/distribute.sh
+++ non-nix/distribute.sh
@@ -4,4 +4,5 @@ ln -s $NON_NIX/doom $HOME/.config/doom
 ln -s $NON_NIX/qtile $HOME/.config/qtile
 ln -s $NON_NIX/qutebrowser $HOME/.config/qutebrowser
 ln -s $NON_NIX/qute-containers $HOME/.local/share/qute-containers
-ln -s /home/ugilt/.dotfiles/non-nix//rofi /home/ugilt/.config/rofi
+ln -s /home/ugilt/.dotfiles/non-nix/rofi /home/ugilt/.config/rofi
+ln -s /home/ugilt/.dotfiles/non-nix/nvim /home/ugilt/.config/nvim
